f3ab80b30fff66c6e0b8f318cdf8dbd3a3f48e37
Vincent <xrocxion@gmail.com>
2017-09-11 10:51:55 -0700

    tweaks, header, refactor

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'resources', to: 'welcome#resources'
   get 'contact-us', to: 'welcome#contact-us'
 
+  get 'error', to: 'welcome#error'
+
   resources :articles
 
   root 'welcome#index'
