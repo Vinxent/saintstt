aee674e4f5a75ecd8ab3e3b68f8d9582bd038c45
Vincent <xrocxion@gmail.com>
2017-09-10 23:13:20 -0700

    routes names

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'home', to: 'welcome#index'
+  get 'about', to: 'welcome#about'
+  get 'resources', to: 'welcome#resources'
+  get 'contact-us', to: 'welcome#contact-us'
 
   resources :articles
 
